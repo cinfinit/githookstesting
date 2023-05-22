66e8999f9de17228e0ffb6e03e0a3c518b871ca3
INFINITE <infinite@192.168.1.6>
2023-05-22 22:58:54 +0530

    checkning husky

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="App">
        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-              <p>Counter {count}</p>
+              <p>Counter {count}</p>  zxnckjzxnck
                         sampler
                   <button onClick={increment}>Increment</button>
                       <br />
